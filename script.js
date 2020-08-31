5a2b4cec3583c5dd740e11fc84a6c58dff04c88a
Glitch (hello-webpage) <none>
2020-08-31 22:29:06 +0000

    🍓😹 Checkpoint ./script.js:5316797/101:5864245/470 ./index.html:5864245/129

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// import gdx library 
-<script src="https://unpkg.com/@vernier/godirect/dist/godirect.min.umd.js"></script>
 
 //let gdxDevice = "";
     const selectDeviceBtn = document.querySelector('#select_device');
@@ -9,14 +7,13 @@
         const gdxDevice = await godirect.selectDevice(); 
         // print name, serial number, order code and measurement period
         output.textContent = `\n Connected to `+gdxDevice.name;
-/*  output.textContent += `\n Order Code: `+gdxDevice.orderCode;
-    output.textContent += `\n Order Code: `+gdxDevice.orderCode;
-    output.textContent += `\n Typical Measurement Period: `+gdxDevice.measurementPeriod + ` ms/sample`;
+        output.textContent += `\n Order Code: `+gdxDevice.orderCode;
+        output.textContent += `\n Typical Measurement Period: `+gdxDevice.measurementPeriod + ` ms/sample`;
 
-wait for battery level and battery state and print
+//      wait for battery level and battery state and print
         output.textContent += `\n Battery Level: `+await gdxDevice.getBatteryLevel() +`%`;
         output.textContent += `\n Charging: ` +(await gdxDevice.getChargingState() === 1 ? `yes` : `no`);
-*/
+
       } catch (err) {
         console.error(err);
       }
