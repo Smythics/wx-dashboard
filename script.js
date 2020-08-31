d70f0f6d7f24a7918b48543f71045ceff07299f1
Glitch (hello-webpage) <none>
2020-08-31 21:43:59 +0000

    🎺💕 Checkpoint ./script.js:5316797/130

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 
-let gdxDevice = "";
+//let gdxDevice = "";
     const selectDeviceBtn = document.querySelector('#select_device');
     const output = document.querySelector('#output');
-
     const selectDevice = async () => {
       try {
-  gdxDevice = await godirect.selectDevice(); 
+        const gdxDevice = await godirect.selectDevice(); 
         // print name, serial number, order code and measurement period
-  output.textContent = `\n Connected to `+gdxDevice.name;
-   //print serial number
+        output.textContent = `\n Connected to `+gdxDevice.name;
+/*  output.textContent += `\n Order Code: `+gdxDevice.orderCode;
   output.textContent += `\n Order Code: `+gdxDevice.orderCode;
-        output.textContent += `\n Typical Measurement Period: `+gdxDevice.measurementPeriod + ` ms/sample`;
+  output.textContent += `\n Typical Measurement Period: `+gdxDevice.measurementPeriod + ` ms/sample`;
 
-// wait for battery level and battery state and print
+wait for battery level and battery state and print
         output.textContent += `\n Battery Level: `+await gdxDevice.getBatteryLevel() +`%`;
         output.textContent += `\n Charging: ` +(await gdxDevice.getChargingState() === 1 ? `yes` : `no`);
-
+*/
       } catch (err) {
         console.error(err);
       }
