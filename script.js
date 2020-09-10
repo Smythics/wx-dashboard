97a472a3fb6bfc77ef30eaa9e3f6d6a309c1e379
Glitch (hello-webpage) <none>
2020-09-10 18:23:40 +0000

    💷🐸 Checkpoint ./script.js:5316797/3063

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-    const selectDeviceBtn = document.querySelector('#select_device');
-    const output = document.querySelector('#output');
-    const cutDeviceBtn = document.querySelector("#cut_device");
+const selectDeviceBtn = document.querySelector("#select_device");
+const output = document.querySelector("#output");
+const cutDeviceBtn = document.querySelector("#cut_device");
 
+const selectDevice = async () => {
+  try {
+    const gdxDevice = await godirect.selectDevice();
+    // print name and serial number
+    output.textContent = `\n Connected to ` + gdxDevice.name;
 
-    const selectDevice = async () => {
-      try {
-        const gdxDevice = await godirect.selectDevice(); 
-        // print name and serial number 
-        output.textContent = `\n Connected to `+gdxDevice.name;
-        
-        //turns on the Default sensor
-        gdxDevice.enableDefaultSensors();
-        //create a constant enabledSensors that correlates with the enabled sensors of the gdx Device
-        const enabledSensors = gdxDevice.sensors.filter(s => s.enabled );
+    //turns on the Default sensor
+    gdxDevice.enableDefaultSensors();
+    //create a constant enabledSensors that correlates with the enabled sensors of the gdx Device
+    const enabledSensors = gdxDevice.sensors.filter(s => s.enabled);
 
-    //create a function that runs for each of the enabled sensor measurements    
-        enabledSensors.forEach(sensor => {
-//trigger a set of actions to occur whenever the value the sensor detects changes
-          sensor.on('value-changed', (sensor) => {
-            document.getElementById('data').innerHTML = `\n ${sensor.value.toFixed(3)} ${sensor.unit}`;
-          sensor.on('value-changed', (sensor) => {
-
-            console.log("sensor on");
-          });
+    //create a function that runs for each of the enabled sensor measurements
+    enabledSensors.forEach(sensor => {
+      //trigger a set of actions to occur whenever the value the sensor detects changes
+      sensor.on("value-changed", sensor => {
+        document.getElementById("data").innerHTML = `\n ${sensor.value.toFixed(
+          3
+        )} ${sensor.unit}`;
+        sensor.on("value-changed", sensor => {
+          console.log("sensor on");
         });
-        }
-      catch (err) {
-        console.error(err);
-      }
-      };
+      });
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
 const cutDevice = async () => {
   gdxDevice.close();
-   sensor.on('value-changed', (sensor) => {
-      console.log("sensor off");
-      )}
+  sensor.on("value-changed", sensor => {
+    console.log("sensor off");
+  });
 };
 
-    selectDeviceBtn.addEventListener('click', selectDevice);
-    cutDeviceBtn.addEventListener("click", cutDevice);//disconnects sensor device
+selectDeviceBtn.addEventListener("click", selectDevice);
+cutDeviceBtn.addEventListener("click", cutDevice); //disconnects sensor device
