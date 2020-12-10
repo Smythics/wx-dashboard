a487624129513dd176417196df339df9fa4634da
Glitch (hello-webpage) <none>
2020-12-10 00:16:58 +0000

    🍋🌂 Checkpoint ./script.js:5316797/9123

--- script.js
+++ script.js
@@ -12,45 +12,58 @@ const selectDevice = async () => {
     // print name and serial number
     output.textContent = `\n Connected to ` + gdxDevice.name;
     cutDeviceBtn.style.visibility = "visible"; //make button visible to deselect sensor
-    selectDeviceBtn.style.visibility="hidden"; //hide select sensor button
-    sensorData.style.visibility="visible"; //make visible output from sensor
+    selectDeviceBtn.style.visibility = "hidden"; //hide select sensor button
+    sensorData.style.visibility = "visible"; //make visible output from sensor
 
     output.textContent += `\n Availiable sensors: (type the channel number into the alert box) `;
 
-        // enable all sensor to print info
-    let enabledSensors = gdxDevice.sensors.filter(s => s.enabled = true);
-       
-      
+    // enable all sensor to print info
+    let enabledSensors = gdxDevice.sensors.filter(s => (s.enabled = true));
+
     enabledSensors.forEach(sensor => {
-    output.textContent += `\n\n Sensor: ${sensor.name} units: ${sensor.unit} channel: ${sensor.number} `  ;
+      output.textContent += `\n\n Sensor: ${sensor.name} units: ${sensor.unit} channel: ${sensor.number} `;
     });
-    
+
     //create a function that runs for each of the enabled sensor measurements
     enabledSensors.forEach(sensor => {
       //trigger a set of actions to occur whenever the value the sensor detects changes
       sensor.on("value-changed", sensor => {
-        document.getElementById("data").innerHTML = `\n ${sensor.value.toFixed (2)} ${sensor.unit}`;
+        document.getElementById("data").innerHTML = `\n ${sensor.value.toFixed(
+          2
+        )} ${sensor.unit}`;
         sensor.on("value-changed", sensor => {
           console.log("sensor on");
         });
       });
     });
-          // wait 3 seconds before starting the chooseSensor function
-      setTimeout(chooseSensor, 3000); 
-    
+    // wait 3 seconds before starting the chooseSensor function
+    setTimeout(chooseSensor, 3000);
   } catch (err) {
     console.error(err);
   }
 };
-
+function chooseSensor() {
+  try {
+    // prompt the user for a channel input choice
+    const channel = prompt("choose a sensor");
+    // set the desired sensor according to the channel selection
+    output.textContent += `\n\n Selected sensors: `;
+    const sensor = gdxDevice.getSensor(parseInt(channel));
+    // enable the selected sensor channel
+    sensor.setEnabled(true);
+    
+  } catch (err) {
+    console.error(err);
+  }
+}
 const cutDevice = async () => {
   try {
     gdxDevice.close();
     document.getElementById("data").innerHTML = "No Data";
     output.textContent = `\n Disconnected from ` + gdxDevice.name;
-    selectDeviceBtn.style.visibility="visible";
-    cutDeviceBtn.style.visibility="hidden";
-    sensorData.style.visibility="hidden"; 
+    selectDeviceBtn.style.visibility = "visible";
+    cutDeviceBtn.style.visibility = "hidden";
+    sensorData.style.visibility = "hidden";
   } catch (err) {
     console.error(err);
   }
